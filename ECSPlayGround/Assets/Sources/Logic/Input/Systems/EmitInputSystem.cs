953ba234ea2a1feb0b986cc7c6bc6edda6e80b83
tungt <tungt@zonmob.com>
2017-06-15 16:14:29 +0700

    ADD Entitas + Input components

---
+++ ECSPlayGround/Assets/Sources/Logic/Input/Systems/EmitInputSystem.cs
@@ -0,0 +1,47 @@
+﻿using Entitas;
+using UnityEngine;
+
+    public sealed class EmitInputSystem : IExecuteSystem, ICleanupSystem
+    {
+        private readonly InputContext _context;
+        private readonly IGroup<InputEntity> _inputs;
+
+        public EmitInputSystem(Contexts contexts)
+        {
+            _context = contexts.input;
+            _inputs = _context.GetGroup(InputMatcher.Input);
+        }
+
+        public void Execute()
+        {
+            if (UnityEngine.Input.GetKeyDown("b"))
+            {
+                _context.isBurstMode = !_context.isBurstMode;
+            }
+
+            var input = _context.isBurstMode 
+            ? Input.GetMouseButton(0)
+            : Input.GetMouseButtonDown(0);
+
+            if (input)
+            {
+                var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 100);
+                if (hit.collider != null)
+                {
+                    var pos = hit.collider.transform.position;
+
+                    _context.CreateEntity()
+                        .AddInput((int) pos.x, (int) pos.y);
+                }
+            }
+        }
+
+        public void Cleanup()
+        {
+            foreach (var e in _inputs.GetEntities())
+            {
+                e.Destroy();
+            }
+        }
+    }
+
