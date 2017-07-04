be674d1853b0e747450ebe4f592317c629a5b996
Tung <melodyinmyheart 91@gmail.com>
2017-07-04 21:23:19 +0700

    update gameboard system

--- ECSPlayGround/Assets/Sources/Logic/GameBoard/GameBoardLogic.cs
+++ ECSPlayGround/Assets/Sources/Logic/GameBoard/GameBoardLogic.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.Sources.Logic.GameBoard
+
+public static class GameBoardLogic
 {
-    class GameBoardLogic
+    public static int GetNextEmptyRow(GameContext c, IntVector2 position)
     {
+        position.y -= 1;
+        while (position.y >= 0 && c.GetEntitiesWithPosition(position).Count == 0)
+        {
+            position.y -= 1;
+        }
+
+
+        return position.y + 1;
     }
 }
+
--- ECSPlayGround/Assets/Sources/Logic/GameBoard/Systems/FallSystem.cs
+++ ECSPlayGround/Assets/Sources/Logic/GameBoard/Systems/FallSystem.cs
@@ -2,10 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Sources.Logic.GameBoard;
+using Entitas;
 
-namespace Assets.Sources.Logic.GameBoard.Systems
+public class FallSystem : ReactiveSystem<GameEntity>
 {
-    class FallSystem
+    private GameContext context;
+
+    public FallSystem(IContext<GameEntity> context) : base(context)
     {
     }
+
+    public FallSystem(Collector<GameEntity> collector) : base(collector)
+    {
+    }
+
+    protected override Collector<GameEntity> GetTrigger(IContext<GameEntity> context)
+    {
+        return context.CreateCollector(GameMatcher.GameBoardElement, GroupEvent.Removed);
+    }
+
+    protected override bool Filter(GameEntity entity)
+    {
+        return true;
+    }
+
+    protected override void Execute(List<GameEntity> entities)
+    {
+        var gameBoard = context.gameBoard;
+        for (int col = 0; col < gameBoard.cols; col++)
+        {
+            for (int row = 1; row < gameBoard.rows; row++)
+            {
+                var pos = new IntVector2(col, row);
+                var moveable = context
+                    .GetEntitiesWithPosition(pos)
+                    .Where(e => e.isMovable)
+                    .ToArray();
+
+                foreach (var e in moveable)
+                {
+                    moveDown(e, pos);
+                }
+            }
+        }
+    }
+
+    private void moveDown(GameEntity e, IntVector2 position)
+    {
+        var nextRow = GameBoardLogic.GetNextEmptyRow(context, position);
+        if (nextRow != position.y)
+        {
+            e.ReplacePosition(new IntVector2(position.x, nextRow));
+        }
+    }
 }
+
