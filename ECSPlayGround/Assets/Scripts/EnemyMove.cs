c4ab62ba44fcf3116dff0653f7e76dc3328f1234
tungt <tungt@zonmob.com>
2017-07-17 16:36:57 +0700

    update enemy move

---
+++ ECSPlayGround/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DG.Tweening;
+using UnityEngine;
+
+class EnemyMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+    
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        var waypoint = WaypointExtension.waypoints;
+        transform.localPosition = waypoint[0];
+
+        transform.DOLocalPath(WaypointExtension.waypoints, speed, PathType.Linear, PathMode.Full3D, 10, Color.green).SetEase(Ease.Linear);
+    }
+}
---
+++ ECSPlayGround/Assets/Scripts/WaypointExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class WaypointExtension
+{
+    public static Vector3[] waypoints = new[] {
+        new Vector3(0f, 2f, 52.61f),
+        new Vector3(0f, 2f, 2.1f),
+        new Vector3(-1.9f, 2f, -0.11f),
+        new Vector3(-13.68f, 2f, 0f),
+        new Vector3(-14.2f, 2f, 38f),
+        new Vector3(-18.72f, 2f, 38.2f),
+        new Vector3(-18.78f, 2f, 47.02f),
+        new Vector3(-42.44f, 2f, 47.4f),
+        new Vector3(-42.9f, 2f, 52f),
+        new Vector3(-62.3357f, 2f, 52f),
+        new Vector3(-62f, 2f, 38.05895f),
+        new Vector3(-52.56f, 2f, 38.14f),
+        new Vector3(-52.6f, 2f, 33.4f),
+        new Vector3(-33.42f, 2f, 33.4f),
+        new Vector3(-33.4f, 2f, 23.48f),
+        new Vector3(-29.44f, 2f, 23.4f),
+        new Vector3(-29.4f, 2f, 0f),
+        new Vector3(-47.72f, 2f, 0f),
+        new Vector3(-47.72f, 2f, 22.88f),
+        new Vector3(-62.03f, 2f, 22.88f),
+        new Vector3(-62.03f, 2f, -4.42f) };
+}
